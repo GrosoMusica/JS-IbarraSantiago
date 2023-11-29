f7c43f2534f4782d474ec05118418bc95dfe953a
Santiago D Ibarra <santiagodanielibarra@gmail.com>
2023-11-29 18:15:26 -0300

    Entrega 3 - CALCULADORA DE PRESUPUESTOS - Santiago D Ibarra

--- js/main.js
+++ js/main.js
@@ -1,131 +1,127 @@
 
-// --- --- --- --- --- --- --- BIENVENIDA
+// REINICIAR
 
-let nombre = prompt("Cómo te llamás??");
+const reinicio = document.getElementById("reinicio");
 
-// VARIABLES y CONSTANTES
+reinicio.addEventListener("click", reiniciarPrograma);
 
-const IVA = 1.21
-const INFLASI = 0.14
-const TIPO_A = 1.666
+function reiniciarPrograma() {
+    
+    localStorage.clear();
 
-let empleados = 1
+    const caja = document.getElementById("caja-calc");
+    caja.style.display = "none";
 
-
-// ARRAY
-
-const servicios = ["Facturación de personajes tipo A", "PYMES", "Profesionales", "los tengo en negro"];
-
-
-console.log( "Hola " +  nombre  + " Bienvenido a los Servicios de Liquidación de Personajes " );
-
-
-console.log(listadoServicios())
-
-
-
-// MÉTODOS DE BÚSQUEDA ARRAY --- --- --- --- // LISTA DE PRECIOS - FOR
-
-
-console.log(" -- --- -- --- -- LISTA DE PRECIOS -- --- -- --- -- ");
-
-for (let i = 1 ; i <= 8 ; i+=1 ) {
-        let precio =  i * 4 ;
-        console.log("el precio por " + i + " empleados es de u$s " + precio + "dólares estadounidenses");
+    Swal.fire({
+        title: "En hora buena",
+        text: "Storage ha sido borrado exitosamente",
+        icon: "success"
+    });
 }
 
+// COMENZAR
 
-// FUNCIONES
+const empezar = document.getElementById("empezar");
 
-function listadoServicios() {
-    console.log("-- --- -- --- -- LISTA DE SERVICIOS -- --- -- --- -- ");
+empezar.addEventListener("click", empezarPresupuesto);
 
-    servicios.forEach((servicio, index) => {
+function empezarPresupuesto() {
     
-        console.log(`${index + 1}. ${servicio}`)
-    })
+    const caja = document.getElementById("caja-calc");
+
+    caja.style.display = "block";
+    
+}
+
+// DARK MODE
+
+const colorModeButton = document.querySelector("#color-mode");
+const body = document.body;
+
+colorModeButton.addEventListener("click", cambiarModoColor);
+
+function cambiarModoColor() {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        colorModeButton.innerText = "LIGHT MODE";
+    } else {
+        colorModeButton.innerText = "DARK MODE";
+    }
+}
+
+// --- --- --- --- VARIABLES --- --- --- --- //
+
+
+// IMPUESTOS
+
+const IVA = 1.21;
+const TIPO_A = 1.33;
+const Desc = 0.95;
+
+// ENTRADAS
+
+let precioFactura = 1.75;
+let subtotal;
+
+const catDesc = ["Estatales", "Autónomo", "Fundación"];
+const catTipoA = ["Multinacional", "Empresas"];
+
+// SELECCIONAR CATEGORIA
+const categoria = document.querySelector("#categoria");
+let categoriaSeleccionada = localStorage.getItem('categoriaSeleccionada') || '';
+
+categoria.value = categoriaSeleccionada;
+
+categoria.addEventListener("change", () => {
+    categoriaSeleccionada = categoria.value;
+    localStorage.setItem('categoriaSeleccionada', categoriaSeleccionada);
+    calcularSubtotal();
+});
+
+// EMPLEADOS
+const empleados = document.querySelector("#empleados");
+let cantidadEmpleados = localStorage.getItem('cantidadEmpleados') || '';
+
+empleados.value = cantidadEmpleados;
+
+empleados.addEventListener("input", () => {
+    cantidadEmpleados = empleados.value;
+    localStorage.setItem('cantidadEmpleados', cantidadEmpleados);
+    calcularSubtotal();
+});
+
+// FACTURAS
+const facturas = document.querySelector("#facturas");
+let cantidadFacturas = localStorage.getItem('cantidadFacturas') || '';
+
+facturas.value = cantidadFacturas;
+
+facturas.addEventListener("input", () => {
+    cantidadFacturas = facturas.value;
+    localStorage.setItem('cantidadFacturas', cantidadFacturas);
+    calcularSubtotal();
+    });
+
+function calcularBonif() {
+    if (catDesc.includes(categoriaSeleccionada)) {
+    bonif = 1 * Desc
+    } else if (catTipoA.includes(categoriaSeleccionada)) {
+    bonif = 1 * TIPO_A;
+        } else {
+    bonif = 1
+    }};
+
+function calcularSubtotal() {
+    subtotal = precioFactura * cantidadEmpleados * cantidadFacturas;
+    document.getElementById('subtotal').textContent = subtotal + " U$S";
+}
+
+const presupuesto = document.getElementById("presupuesto");
+presupuesto.addEventListener("click", calcularTotal);
+
+function calcularTotal() {
+    calcularBonif();
+    total = subtotal * IVA * bonif;
+
+    document.getElementById('total').textContent = total.toFixed(2) + " U$S";
 };
-
-
-// // --- --- --- --- --- --- --- CAPTURAR ENTRADAS - PROMPT ---- --- --- --- --- --- //
-
-
-alert("Bienvenido a los Servicios de Liquidación de Personajes")
-
-empleados = Number(prompt("¿A cuántos empleados tienes que facturar hoy?"));
-
-while (empleados > 8) {
-    
-        alert("no trabajo con mas de 8 personajes!!!")
-    
-    empleados = Number(prompt("Puedo facturar hasta 8"))
-}
-
-
-alert("ALTO QUEREMOS SABER MAS DE VOS")
-
-let seleccion = prompt("Seleccione el número del servicio que desea:");
-
-while (seleccion < 1 || seleccion > 4) {
-    alert("No es una opción válida. Por favor, ingrese un número del 1 al 4.");
-    seleccion = prompt("Seleccione el número del servicio que desea:");
-}
-
-// --- --- --- --- --- --- --- --- PRESUPUESTO --- --- --- --- --- //
-
-
-let precio = empleados * 4;
-
-
-if (seleccion == 1) {
-    
-    // Personajes Tipo A
-    subtotal = precio * TIPO_A;
-} else if (seleccion == 2) {
-    
-    // PYMES
-    subtotal = precio * IVA * (1 + INFLASI);
-} else if (seleccion == 3) {
-    
-    // Profesionales
-    subtotal = precio * IVA;
-} else if (seleccion == 4) {
-    
-    // Negro
-    subtotal = precio * (1 + INFLASI);
-}
-
-// --- --- --- --- --- --- --- --- IMPUESTOS --- --- --- --- --- //
-
-let impuestos = (subtotal - precio);
-
-function iva() {
-    
-    console.log("El IVA es de u$d " + impuestos.toFixed(2));
-}
-    if (impuestos > 7) {
-        iva();
-        alert("tus impuestos estan pasados de rosca!!!");
-}
-
-// OBJETOS
-
-let cliente = {
-    nombre: nombre,
-    servicioElegido: servicios[seleccion - 1],
-    cantidadEmpleados: empleados,
-    precioTotal: subtotal.toFixed(2),
-    impuestos: impuestos.toFixed(2)
-};
-
-// --- --- --- --- --- --- --- --- -- MOSTRAR RESULTADOS --- --- --- --- -- --- --- //
-
-console.log("-- --- -- --- -- INFORMACIÓN DEL CLIENTE -- --- -- --- -- ");
-
-console.log("Nombre:", cliente.nombre);
-console.log("Tipo de Servicio:", cliente.servicioElegido);
-console.log("Cantidad de Empleados:", cliente.cantidadEmpleados);
-console.log("IVA e impuestos =", cliente.impuestos);
-console.log("Total: u$s", cliente.precioTotal);
-
-// -----------------------------------------------------------------------

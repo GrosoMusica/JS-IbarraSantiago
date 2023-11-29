785f7dc9d80d44653cd4e29af6d1e9062ef6dcb1
Santiago D Ibarra <santiagodanielibarra@gmail.com>
2023-11-29 17:09:21 -0300

    JavaScript - Events

--- js/main1.js
+++ js/main1.js
@@ -1,11 +1,39 @@
 
-// Agregar clase a un elemento -- Switch Color Mode
+// REINICIAR STORAGE
 
+const reinicio = document.getElementById("reinicio");
+
+reinicio.addEventListener("click", reiniciarPrograma);
+
+function reiniciarPrograma() {
+    
+    localStorage.clear();
+    Swal.fire({
+        title: "En hora buena",
+        text: "Storage ha sido borrado exitosamente",
+        icon: "success"
+    });
+}
+
+// COMENZAR
+
+const empezar = document.getElementById("empezar");
+
+empezar.addEventListener("click", empezarPresupuesto);
+
+function empezarPresupuesto() {
+    
+    alert("Pienvenido a WPress Calc");
+    
+}
+
+// DARK MODE
 
 const colorModeButton = document.querySelector("#color-mode");
 const body = document.body;
 
 colorModeButton.addEventListener("click", cambiarModoColor);
+
 function cambiarModoColor() {
     body.classList.toggle("dark-mode");
     if (body.classList.contains("dark-mode")) {
@@ -15,10 +43,147 @@ function cambiarModoColor() {
     }
 }
 
+// --- --- --- --- VARIABLES --- --- --- --- //
+
+
+
 // IMPUESTOS
 
 const IVA = 1.21
+const TIPO_A = 1.666
 const Desc = 0.95
+
+// ENTRADAS
+
+let precioFactura = 1.75;
+let subtotal;
+
+
+const catDesc = ["Estatales", "Autónomo", "Fundación"];
+const catTipoA = ["Multinacional", "Empresas"];
+
+
+
+// SELECCIONAR CATEGORIA
+const categoria = document.querySelector("#categoria");
+let categoriaSeleccionada = localStorage.getItem('categoriaSeleccionada') || '';
+
+categoria.value = categoriaSeleccionada;
+
+categoria.addEventListener("change", () => {
+    categoriaSeleccionada = categoria.value;
+    localStorage.setItem('categoriaSeleccionada', categoriaSeleccionada);
+    calcularSubtotal();
+});
+
+// EMPLEADOS
+const empleados = document.querySelector("#empleados");
+let cantidadEmpleados = localStorage.getItem('cantidadEmpleados') || '';
+
+empleados.value = cantidadEmpleados;
+
+empleados.addEventListener("input", () => {
+    cantidadEmpleados = empleados.value;
+    localStorage.setItem('cantidadEmpleados', cantidadEmpleados);
+    calcularSubtotal();
+});
+
+// FACTURAS
+const facturas = document.querySelector("#facturas");
+let cantidadFacturas = localStorage.getItem('cantidadFacturas') || '';
+
+facturas.value = cantidadFacturas;
+
+facturas.addEventListener("input", () => {
+    cantidadFacturas = facturas.value;
+    localStorage.setItem('cantidadFacturas', cantidadFacturas);
+    calcularSubtotal();
+    });
+
+function calcularBonif() {
+    if (catDesc.includes(categoriaSeleccionada)) {
+    bonif = 1 * Desc
+    } else if (catTipoA.includes(categoriaSeleccionada)) {
+    bonif = 1 * TIPO_A;
+        } else {
+    bonif = 1
+    }};
+
+
+function calcularSubtotal() {
+    subtotal = precioFactura * cantidadEmpleados * cantidadFacturas;
+    document.getElementById('subtotal').textContent = subtotal + " U$S";
+}
+
+
+
+const presupuesto = document.getElementById("presupuesto");
+
+presupuesto.addEventListener("click", calcularTotal());
+
+
+function calcularTotal() {
+    calcularBonif();
+    total = subtotal * IVA * bonif 
+    document.getElementById('total').textContent = total + " U$S";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+let precioFactura = 0.75
+let subtotal
+
+
+
+// SELECCIONAR CATEGORIA
+const categoria = document.querySelector("#categoria");
+categoria.addEventListener("change", () => {
+    categoriaSeleccionada = categoria.value;
+});
+
+// EMPLEADOS
+const empleados = document.querySelector("#empleados");
+empleados.addEventListener("input", () => {
+    cantidadEmpleados = empleados.value;
+});
+
+
+// FACTURAS
+const facturas = document.querySelector("#facturas");
+facturas.addEventListener("input", () => {
+    cantidadFacturas = facturas.value;
+});
+
+
+
+function calcularSubtotal() {
+    Number.subtotal = cantidadEmpleados * cantidadFacturas * precioFactura;
+}
+
+document.getElementById('subtotal').textContent = subtotal;
+
+
+
+
+
+
+
 
 
 
@@ -46,5 +211,5 @@ document.getElementById('btnPresupuesto').addEventListener('click', function() {
     totalSpan.textContent = total;
 });
 
-
+*/
 
